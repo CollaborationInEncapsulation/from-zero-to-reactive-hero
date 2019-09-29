26610972cd8c8e7d0cbade0203ea2b2e3f8a03f4
Igor Lozynskyi <dawon@i.ua>
2019-09-30 00:59:32 +0300

    Updated reactor version

--- work/chapter-2/src/main/java/com/example/part_10/utils/NettyUtils.java
+++ work/chapter-2/src/main/java/com/example/part_10/utils/NettyUtils.java
@@ -1,11 +1,12 @@
 package com.example.part_10.utils;
 
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
 import reactor.netty.http.websocket.WebsocketInbound;
 import reactor.netty.http.websocket.WebsocketOutbound;
 
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.function.Function;
 
 public class NettyUtils {
@@ -13,12 +14,11 @@ public class NettyUtils {
     public static Flux<String> prepareInput(WebsocketInbound inbound) {
         return inbound
                 .receiveFrames()
-                .map(hc -> hc.content().toString(Charset.forName("UTF-8")));
+                .map(hc -> hc.content().toString(StandardCharsets.UTF_8));
     }
 
     public static Function<Flux<String>, Publisher<Void>> prepareOutbound(WebsocketOutbound outbound) {
-        return output -> outbound.
-                options(s -> s.flushOnEach(true))
-                .sendString(output, Charset.forName("UTF-8"));
+        return output -> outbound
+                .sendObject(output.map(TextWebSocketFrame::new), __ -> true);
     }
 }
